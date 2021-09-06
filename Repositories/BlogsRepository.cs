11cf2e7e6cae59f49fd5217cce516965e8131dbe
cd kingdom <aaronpeet@icloud.com>
2021-09-06 16:44:30 -0600

    Controller, service and repository for getall blog

--- Repositories/BlogsRepository.cs
+++ Repositories/BlogsRepository.cs
@@ -1,7 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using wk10checkpoint.Models;
+using Dapper;
+
 namespace wk10checkpoint.Repositories
 {
     public class BlogsRepository
     {
+        private readonly IDbConnection _db;
+        public BlogsRepository(IDbConnection db)
+        {
+            _db = db;
+        }
+        internal List<Blog> Get()
+        {
+            string sql = @"
+            SELECT
+            a.*,
+            b.*
+            FROM blogs b
+            JOIN accounts a ON b.creatorId = a.id
+            ";
+            return _db.Query<Profile, Blog, Blog>(sql, (profile, blog) =>
+            {
+                blog.CreatorId = profile;
+                return blog;
+            }, splitOn: "id").ToList();
+        }
         
     }
 }
